30ecf080f52c9aba1659497d64858b365750487e
Williano <paawilly17@gmail.com>
2018-12-10 15:08:15 +0000

    [ADD NEW CODE] :white_check_mark: Add second vue project

---
+++ Vuejs/calendar_app/src/app/components/CalendarWeek.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="calendar-week" class="container">
+      <div class="columns is-mobile">
+
+        <CalendarDay v-for="day in sharedState.seedData"
+        v-bind:key='day.id'
+        v-bind:key="day"
+        />
+
+      </div>
+    </div>
+	
+</template>
+
+
+<script>
+	import { store } from '../store.js';
+	import CalendarDay from './CalendarDay.vue'; 
+
+	export default {
+		name: 'CalendarWeek',
+		data () {
+			return {
+				sharedState: store.state 
+			}
+		},
+		components: {
+			CalendarDay
+		}
+	}
+
+</script>
+
+
+<style lang="scss" scoped>
+	
+#calendar-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+
+</style>
